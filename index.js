aa9411d366dc6fdf66acacff70a06293e002ecba
Матвей Васюра <matveyvasyra26@gmail.com>
2021-05-17 23:18:26 +0300

    Added animations for keys

--- index.js
+++ index.js
@@ -27,25 +27,58 @@
       switch (e.key) {
         case "a":
           player.start(SOUNDS[0])
-          buttonA.click()
+          buttonA.className = "keyActive"
           break
         case "s":
           player.start(SOUNDS[1])
+          buttonS.className = "keyActive"
           break
         case "d":
           player.start(SOUNDS[2])
+          buttonD.className = "keyActive"
           break
         case "f":
           player.start(SOUNDS[3])
+          buttonF.className = "keyActive"
           break
         case "g":
           player.start(SOUNDS[4])
+          buttonG.className = "keyActive"
           break
         case "h":
           player.start(SOUNDS[5])
+          buttonH.className = "keyActive"
           break
         case "j":
           player.start(SOUNDS[6])
+          buttonJ.className = "keyActive"
+        default:
+          break
+      }
+    })
+
+    window.addEventListener("keyup", e => {
+      switch (e.key) {
+        case "a":
+          buttonA.className = "key"
+          break
+        case "s":
+          buttonS.className = "key"
+          break
+        case "d":
+          buttonD.className = "key"
+          break
+        case "f":
+          buttonF.className = "key"
+          break
+        case "g":
+          buttonG.className = "key"
+          break
+        case "h":
+          buttonH.className = "key"
+          break
+        case "j":
+          buttonJ.className = "key"
         default:
           break
       }
